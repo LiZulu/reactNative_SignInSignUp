3b64b1e5061d2a62a552a0ab6ef322c1d85aced1
Li Zulu <nurliyanakai.zuliskandar@gmail.com>
2025-04-14 06:49:03 +0800

    commit
    made alterations to sign up and log in page

--- src/components/screens/SignUpScreen.js
+++ src/components/screens/SignUpScreen.js
@@ -3,11 +3,11 @@ import React from 'react'
 import Animated, { FadeIn, FadeInDown, FadeInUp, FadeOut } from 'react-native-reanimated';
 import { useNavigation } from '@react-navigation/native';
 
-export default function SignUpScreen() {
-  
-  const navigation = useNavigation();
-  
-  return (
+export default function LoginScreen() {
+
+    const navigation = useNavigation();
+    
+    return (
     <View style={styles.container}>
       <Image
         style={styles.backgroundImg}
@@ -17,47 +17,56 @@ export default function SignUpScreen() {
         <View style={styles.iconContainer}>
             <Animated.Image entering={FadeInUp.delay(200).duration(1000).springify().damping(3)}
             style={styles.image1}
-            source={require('../../assets/images/christmas-ball.png')} />
+            source={require('../../assets/images/dumbbell.png')} />
             <Animated.Image entering={FadeInUp.delay(200).duration(1000).springify()}
             style={styles.image2}
-            source={require('../../assets/images/christmas-ball2.png')} />
+            source={require('../../assets/images/kettlebell.png')} />
         </View>
 
         {/** title and form **/}
         <View style={styles.form}>
-          {/** title **/}
-          <View style={styles.formContent1}> 
-            <Animated.Text entering={FadeInUp.duration(1000).springify()} style={styles.formTitle}>
-              Sign Up
-            </Animated.Text>
-          </View>
+            {/** title **/}
+            <View style={styles.formContent1}> 
+                <Animated.Text entering={FadeInUp.duration(1000).springify()} style={styles.formTitle}>
+                    Register
+                </Animated.Text>
+            </View>
 
-          {/** form **/}
-          <View style={styles.formContent2}>
-              <Animated.View entering={FadeInUp.duration(1000).springify()} style={styles.formEmail}>
-                <TextInput placeholder='Username' placeholderTextColor={'white'} />
-              </Animated.View>
+            {/** form **/}
+            <View style={styles.formContent2}> 
+                <Animated.View entering={FadeInUp.duration(1000).springify()}>
+                    <View style={styles.formUsername}>
+                        <TextInput placeholder='Username' placeholderTextColor={'white'} />
+                    </View>
+                </Animated.View>
                 
-              <Animated.View entering={FadeInUp.delay(200).springify()} style={styles.formEmail}>
-                <TextInput placeholder='Email' placeholderTextColor={'white'} />
-              </Animated.View>
+                <Animated.View entering={FadeInUp.duration(1000).springify()}>
+                    <View style={styles.formEmail}>
+                        <TextInput placeholder='Email' placeholderTextColor={'white'} />
+                    </View>
+                </Animated.View>
 
-              <Animated.View entering={FadeInUp.delay(400).springify()} style={styles.formPassword}>
-                <TextInput placeholder='Password' placeholderTextColor={'white'} />
-              </Animated.View>
+                <Animated.View entering={FadeInUp.delay(200).springify()}>
+                    <View style={styles.formPassword}>
+                        <TextInput placeholder='Password' placeholderTextColor={'white'} />
+                    </View>
+                </Animated.View>
 
-              <Animated.View entering={FadeInUp.delay(600).springify()} style={styles.formButton}>
-                <TouchableOpacity
-                  style={styles.formButtonStyle}>
-                  <Text style={styles.buttonText}> Sign Up </Text>
-                </TouchableOpacity>
-              </Animated.View>
+                <Animated.View entering={FadeInUp.delay(400).springify()}>
+                    <View style={styles.formButton}>
+                        <TouchableOpacity style={styles.formButtonStyle}>
+                            <Text style={styles.buttonText}> Sign Up </Text>
+                        </TouchableOpacity>
+                    </View>
+                </Animated.View>
 
-                <Animated.View entering={FadeInDown.delay(800).springify()} style={styles.formAccountRegister}>
-                    <Text style={styles.registerLinkText}> Already have an account? </Text>
-                    <TouchableOpacity onPress={() => navigation.push('Login')}>
-                        <Text style={styles.linkSignUp}> Login </Text>
-                    </TouchableOpacity>
+                <Animated.View entering={FadeInDown.delay(600).springify()}>
+                    <View style={styles.formAccountRegister}>
+                        <Text style={styles.registerLinkText}> Already have an account? </Text>
+                        <TouchableOpacity onPress={() => navigation.push('Login')}>
+                            <Text style={styles.linkSignUp}> Log In </Text>
+                        </TouchableOpacity>
+                    </View>
                 </Animated.View>
             </View>
         </View>
@@ -66,134 +75,144 @@ export default function SignUpScreen() {
 }
 
 const styles = StyleSheet.create({
-  container: 
-  {
-      flex: 1,
-      backgroundColor: 'white',
-  },
+    container: 
+    {
+        flex: 1,
+        backgroundColor: 'white',
+    },
 
-  backgroundImg: {
-      position: 'absolute',
-      width: '100%',
-      height: '110%',
-      marginTop: -100,
-  },
+    backgroundImg: {
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        marginTop: 0,
+    },
 
-  iconContainer:
-  {
-      flexDirection: 'row',
-      justifyContent: 'center',
-      width: '100%',
-      position: 'absolute',
+    iconContainer:
+    {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        width: '100%',
+        position: 'absolute',
+    },
 
-  },
+    image1: {
+        height: '180',
+        width: '160',
+        marginHorizontal: 20,
+    },
 
-  image1: {
-      height: '180',
-      width: '160',
-      marginHorizontal: 20,
-  },
+    image2: {
+        height: '200',
+        width: '180',
+        marginHorizontal: 20,
+        marginTop: -10,
+    },
 
-  image2: {
-      height: '200',
-      width: '180',
-      marginHorizontal: 20,
-      marginTop: -10,
-  },
+    form: {
+        width: '100%',
+        height: '100%',
+        flex: 1,
+        justifyContent: 'flex-start', // Changed to flex-start to align content at the top
+        paddingBottom: 340,
+    },
 
-  form: {
-      width: '100%',
-      height: '100%',
-      flex: 1,
-      justifyContent: 'center',
-      paddingBottom: 270,
-  },
+    formContent1:
+    {
+        flex: 1,
+        alignItems: 'center',
+    },
 
-  formContent1:
-  {
-      flex: 1,
-      alignItems: 'center',
-  },
+    formContent2:
+    {
+        flex: 1,
+        alignItems: 'center',
+        marginLeft: 4,
+        marginRight: 4,
+        marginTop: 10,  // Adjusted for better spacing
+        letterSpacing: 4,
+    },
 
-  formContent2:
-  {
-      flex: 1,
-      alignItems: 'center',
-      marginLeft: 4,
-      marginRight: 4,
-      marginTop: 1, /* 16px */
-      letterSpacing: 4,
-  },
+    formTitle:
+    {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 60,
+        marginTop: 200, // Reduced margin to ensure title and form don't overlap
+        letterSpacing: 4,
+    },
 
-  formTitle:
-  {
-      color: 'white',
-      fontWeight: 'bold',
-      fontSize: 60,
-      marginTop: 280, // Space between the title and form
-      letterSpacing: 4,
-  },
+    formUsername:
+    {
+        backgroundColor: 'gray',
+        padding: 10,
+        borderRadius: 16,
+        width: '100%',
+        opacity: 0.7,
+        marginBottom: 25, // Space below the Email input
+    },
 
-  formEmail:
-  {
-      backgroundColor: 'gray',
-      padding: 10,
-      borderRadius: 16,
-      width: '100%',
-      opacity: 0.7,
-      marginBottom: 25, // Space below the Email input
-  },
 
-  formPassword:
-  {
-      backgroundColor: 'gray',
-      padding: 10,
-      borderRadius: 16,
-      width: '100%',
-      opacity: 0.7,
-      marginBottom: 30,
-  },
+    formEmail:
+    {
+        backgroundColor: 'gray',
+        padding: 10,
+        borderRadius: 16,
+        width: '100%',
+        opacity: 0.7,
+        marginBottom: 25, // Space below the Email input
+    },
 
-  formButton:
-  {
-      width: '100%',
-  },
+    formPassword:
+    {
+        backgroundColor: 'gray',
+        padding: 10,
+        borderRadius: 16,
+        width: '100%',
+        opacity: 0.7,
+        marginBottom: 30,
+    },
 
-  formButtonStyle:
-  {
-      width: '100%',
-      backgroundColor: '#76a797',
-      borderRadius: 10,
-      color: 'white',
-      padding: 16,
-      marginBottom: 5,
-  },
+    formButton:
+    {
+        width: '100%',
+    },
 
-  buttonText:
-  {
-      fontSize: 20,
-      fontWeight: 'bold',
-      color: 'white',
-      textAlign: 'center',
-  },
+    formButtonStyle:
+    {
+        width: '100%',
+        backgroundColor: '#7DF9FF',
+        borderRadius: 10,
+        color: 'white',
+        padding: 16,
+        marginBottom: 5,
+    },
 
-  formAccountRegister:
-  {
-      flexDirection: 'row',
-      textAlign: 'justify',
-      marginTop: 10,
-  },
+    buttonText:
+    {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'white',
+        textAlign: 'center',
+    },
 
-  registerLinkText:
-  {
-      color: 'white',
-      fontSize: 20,
-  },
+    formAccountRegister:
+    {
+        flexDirection: 'row',
+        textAlign: 'justify',
+        marginTop: 10,
+    },
 
-  linkSignUp:
-  {
-      fontSize: 20,
-      color: '#76a797',
-      fontWeight: 'bold',
-  }
+    registerLinkText:
+    {
+        color: 'white',
+        fontSize: 20,
+    },
+
+    linkSignUp:
+    {
+        fontSize: 20,
+        color: '#7DF9FF',
+        fontWeight: 'bold',
+    }
 });
